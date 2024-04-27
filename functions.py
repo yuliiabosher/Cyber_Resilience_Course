ad2df2eb8a717b908c9915d70c80e9a365342840
Yuliia <yulia.bosher@gmail.com>
2024-04-27 18:09:10 +0100

    added unzipped cloud files

--- functions.py
+++ functions.py
@@ -1,12 +1,74 @@
 import os
 import boto3
+import json
+import csv
+import botocore
 
-def get_S3_client():
-  resource = boto3.client("s3")
-  return resource
-def get_file(client, filename):
-  file_object = client.get_object(Bucket=os.environ.get('BUCKET_NAME'), Key=filename)
-  data_file = file_object['Body'].read()
-  return str(data_file.decode('utf-8'))
-
-  
+def add_school_data_to_bucket(client, filename, filedata):
+    try:
+        s3_client = boto3.client('s3')
+        s3 = boto3.resource('s3')
+        # access the bucket using the environment variable
+        bucket = s3.Bucket(os.environ.get("BUCKET_NAME"))
+        # read the file from the bucket 
+        obj = s3_client.get_object(Bucket=os.environ.get("BUCKET_NAME"), Key=filename)
+        data = obj['Body'].read().decode('utf-8').splitlines()
+        records = csv.reader(data) 
+        names_array = []
+        # create a file in a temporary folder and write the bucket file contents in it 
+        with open('/tmp/schools_list.csv', 'w', newline='') as f:
+            for row in records:
+                names_array.append(row[0])
+                writer = csv.writer(f)
+                writer.writerow(row)
+        new_data_rows = []
+        # append the schools not in bucket file to the file in the temporary folder 
+        with open('/tmp/schools_list.csv', 'a', newline='') as f:
+            for i in filedata:
+                if i[0] not in names_array:
+                    new_data_rows.append(i)
+                    writer = csv.writer(f)
+                    writer.writerow(i)
+        if len(new_data_rows) == 0:
+            return "These schools are already in a file", []
+        else:
+            # upload the file from the temporary folder to the bucket '''
+            bucket.upload_file('/tmp/schools_list.csv', filename)
+            return "New data been successfully added", new_data_rows
+    except botocore.exceptions.ClientError as e:
+        # if the file doesn't exist create a new file in the temporary folder
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            with open('/tmp/schools_list.csv', 'w', newline='') as f:
+                writer = csv.writer(f)
+                for row in filedata:
+                    writer.writerow(row)
+            # upload the file from the temporary folder to the bucket
+            bucket.upload_file('/tmp/schools_list.csv', filename)
+            return "New file been successfully created", filedata
+        else:
+            return e.response["Error"]["Code"], []
+            
+def show_schools_data_in_bucket(client, filename):
+    try: 
+        s3_client = boto3.client('s3')
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(os.environ.get("BUCKET_NAME"))
+        # read and return data from the bucket 
+        obj = s3_client.get_object(Bucket=os.environ.get("BUCKET_NAME"), Key=filename)
+        data = obj['Body'].read().decode('utf-8').splitlines()
+        records = csv.reader(data) 
+        data_array =[]
+        for row in records:
+            data_array.append(row)
+        return "The data has been found", data_array
+    except botocore.exceptions.ClientError as e:
+        # if there is no file in bucket return a message and an empty list
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            return "There is no such file", []
+        else:
+            return e.response["Error"]["Code"], []
+    except:
+        return "There was an error", []
+    
+    
+    
--- lambda_function.py
+++ lambda_function.py
@@ -1,33 +1,48 @@
+import boto3
 import json
-from functions import get_S3_client, get_file
-
+from functions import add_school_data_to_bucket, show_schools_data_in_bucket
 def lambda_handler(event, context):
-  try:
-    request = event["body"]
-    if type(request) is not dict:
-      request = json.loads(request)
-    data = request["data"]
-    csv_filename = data["csv_filename"]
-    resource = get_S3_client()
-    return_data = get_file(resource, csv_filename)
-    return {
-        "statusCode": 200,
-        "headers":{
-            "Content-Type" : "application/json",
-            "Access-Control-Allow-Headers" : "Content-Type,X-Api-Key",
-            "Access-Control-Allow-Methods" : "POST",
-            "Access-Control-Allow-Origin":"*"
-        },
-        "body": json.dumps(return_data),
-    }
-  except Exception as e:
-    return {
-        "statusCode": 404,
-        "headers":{
-            "Content-Type" : "application/json",
-            "Access-Control-Allow-Headers" : "Content-Type,X-Api-Key",
-            "Access-Control-Allow-Methods" : "POST",
-            "Access-Control-Allow-Origin":"*"
-        },
-        "body": f"there was an error, {e}"
-    }
+    try:
+        filename = "schools_list.csv"
+        client = boto3.client('s3')
+        # the code executed if Postman sends POST request 
+        if event["httpMethod"] == "POST":
+            if "body" in event.keys():
+                request = event["body"]
+                if type(request) is not dict:
+                    request = json.loads(request)
+                if request is not None and "data" in request.keys():
+                    '''if there is no data return a message and an empty list '''
+                    data = request["data"]
+                    if len(data) == 0:
+                        message, return_data = "Please enter valid data", []
+                        statuscode = 404
+                    else:
+                        # if there is data call the function with the data
+                        message, return_data = add_school_data_to_bucket(client, filename, data)
+                        statuscode = 200
+                else:
+                    message, return_data = "Error in the POST request occured", []
+                    statuscode = 404
+        # the code executed if Postman sends GET request
+        elif event["httpMethod"] == "GET":
+            message, return_data = show_schools_data_in_bucket(client, filename)
+            statuscode = 200
+        else:
+            message, return_data = "Error occured", []
+            statuscode = 404
+        return {'statusCode': statuscode,
+                'headers': {'Content-Type': 'application/json',
+                            'Access-Control-Allow-Headers': 'Content-Type,X-Api-Key',
+                            'Access-Control-Allow-Methods': 'POST',
+                            'Access-Control-Allow-Origin': '*'},
+                'body': json.dumps({"message": message, "data": return_data})
+                }
+    except Exception as e:
+        return {'statusCode': 404,
+                'headers': {'Content-Type': 'application/json',
+                            'Access-Control-Allow-Headers': 'Content-Type,X-Api-Key',
+                            'Access-Control-Allow-Methods': 'POST',
+                            'Access-Control-Allow-Origin': '*'},
+                'body': json.dumps({"message": "There was an error", "data": []})
+                }
